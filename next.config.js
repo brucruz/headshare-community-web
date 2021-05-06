d7cecd9a3f80e9f5fa785e359294c24f588fc6bd
Bruno Cruz <bru.cruz@gmail.com>
2021-05-06 19:58:58 -0300

    fix: build error due to json-loader

--- next.config.js
+++ next.config.js
@@ -2,12 +2,4 @@ module.exports = {
   images: {
     domains: ['headshare.s3.amazonaws.com', 'via.placeholder.com'],
   },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.json$/,
-      loader: 'json-loader',
-    });
-
-    return config;
-  },
 };
